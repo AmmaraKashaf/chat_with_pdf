516b6a21553034041643319cc2b4c0634150283e
Ammara Kashaf <ammarakashaf1@gmail.com>
2025-07-11 23:20:21 +0500

    extract_pdf_text.py

--- extract_pdf_text.py
+++ extract_pdf_text.py
@@ -1,15 +1,15 @@
-import fitz  # PyMuPDF
+import fitz 
 
-# PDF file ka name yahan likho
+
 pdf_file = "paper.pdf"
 
-# File open karo
+
 doc = fitz.open(pdf_file)
 
-# Text collect karne ke liye variable
+
 text = ""
 for page in doc:
     text += page.get_text()
 
-# Output dekho
-print(text[:1000])  # sirf pehlay 1000 characters dekho
+
+print(text[:1000])  
